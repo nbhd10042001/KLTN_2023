32644f069f78678d9cd27f46fe393d743552d8aa
nbhd10042001 <nguyenbaohoangduc@gmail.com>
2022-11-29 11:41:14 +0700

    ll

---
+++ detect_car.py
@@ -0,0 +1,43 @@
+import cv2
+from Vehicle_detect import VehicleDetector
+import matplotlib.pyplot as plt
+import glob  
+
+
+# Load vehicle detector
+vd = VehicleDetector()
+
+# Load images from a folder
+images_folder = glob.glob("img/car/*.*") # muon tim cu the thi dung ("img/*.jpg")
+# print(images_folder)
+
+
+# Loop through the images
+for img_path in images_folder:
+    # print("img_path:", img_path)
+
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, [960, 640])
+
+    vehicle_boxes = vd.detect_vehicles(img)
+    # print (vehicle_boxes)
+    vehicle_count = len(vehicle_boxes)
+
+
+    for box in vehicle_boxes:
+        x, y, w, h = box
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+        cv2.putText(img, "Vehicles: " + str(vehicle_count), (20, 50), 0, 2, (0, 255, 0), 2)
+
+        h_i = img.shape[0]
+        #calculate distance
+        dis = ((h_i) / h)
+        cv2.putText(img, "Dis: " + str(round(dis, 2)) + "m", (x, y), 0, 1, (255, 0, 0), 2)
+
+ 
+    cv2.imshow("Car", img)
+    cv2.waitKey(0)
+
+    # plt.imshow(img)
+    # plt.show()
---
+++ detect_car_video.py
@@ -0,0 +1,34 @@
+import cv2
+from Vehicle_detect import VehicleDetector
+
+
+# Load vehicle detector
+vd = VehicleDetector()
+
+# video = cv2.VideoCapture("video/slow_traffic_small.mp4")
+video = cv2.VideoCapture(0)
+
+
+# Loop through the images
+while True:
+
+    _, frame = video.read()
+
+    vehicle_boxes = vd.detect_vehicles(frame)
+    # print (vehicle_boxes)
+    vehicle_count = len(vehicle_boxes)
+
+    for box in vehicle_boxes:
+        x, y, w, h = box
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255,0,0), 2)
+        cv2.putText(frame, "Vehicles: " + str(vehicle_count), (20, 50), 0, 2, (0, 255, 0), 2)
+
+    cv2.imshow("Car", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
